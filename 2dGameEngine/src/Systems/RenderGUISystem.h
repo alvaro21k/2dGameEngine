d9bb0a5e829d88461d8bfd68ca31e19b5bc7d86d
Alvaro Nicosia <alvaro21k@gmail.com>
2025-02-28 12:11:03 +0100

    Added options to enemy creation GUI

--- 2dGameEngine/src/Systems/RenderGUISystem.h
+++ 2dGameEngine/src/Systems/RenderGUISystem.h
@@ -10,6 +10,7 @@
 #include "../Components/BoxColliderComponent.h"
 #include "../Components/ProjectileEmitterComponent.h"
 #include "../Components/HealthComponent.h"
+#include <math.h>
 #include <glm/glm.hpp>
 
 class RenderGUISystem : public System {
@@ -22,21 +23,54 @@ public:
 
 		if (ImGui::Begin("Spawn enemies")) {
 			static int enemyXPos = 0;
-			ImGui::InputInt("enemy x position", &enemyXPos);
-
 			static int enemyYPos = 0;
+			static int enemyXScale = 1;
+			static int enemyYScale = 1;
+			static int enemyXVel = 0;
+			static int enemyYVel = 0;
+			static int enemyHealth = 100;
+			static float rotation = 0.0;
+			static float projAngle = 0.0;
+			static float projSpeed = 100.0;
+			static int projRepeat = 10;
+			static int projDuration = 10;
+			const char* items[]{ "tank-image","truck-image" };
+			static int selection = 0;
+			
+			if (ImGui::CollapsingHeader("Sprite", ImGuiTreeNodeFlags_DefaultOpen)) {
+				ImGui::Combo("Select enemy sprite", &selection, items, IM_ARRAYSIZE(items));
+			}
+			ImGui::Spacing;
+
+			ImGui::InputInt("enemy x position", &enemyXPos);
 			ImGui::InputInt("enemy y position", &enemyYPos);
+			ImGui::InputInt("enemy x scale", &enemyXScale);
+			ImGui::InputInt("enemy y scale", &enemyYScale);
+			ImGui::InputInt("enemy x velocity", &enemyXVel);
+			ImGui::InputInt("enemy y velocity", &enemyYVel);
+			ImGui::SliderInt("enemy health", &enemyHealth, 0, 100);
+			ImGui::SliderAngle("enemy rotation (deg)", &rotation, 0, 360);
+			
 
 			if (ImGui::Button("Create new enemy")) {
 				Entity enemy = registry->CreateEntity();
 				enemy.Group("enemies");
-				enemy.AddComponent<TransformComponent>(glm::vec2(enemyXPos, enemyYPos), glm::vec2(1.0, 1.0), 0.0);
-				enemy.AddComponent<RigidBodyComponent>(glm::vec2(0.0, 0.0));
-				enemy.AddComponent<SpriteComponent>("tank-image", 32, 32, 1);
+				enemy.AddComponent<TransformComponent>(glm::vec2(enemyXPos, enemyYPos), glm::vec2(enemyXScale, enemyYScale), glm::degrees(rotation));
+				enemy.AddComponent<RigidBodyComponent>(glm::vec2(enemyXVel, enemyYVel));
+				enemy.AddComponent<SpriteComponent>(items[selection], 32, 32, 1);
 				enemy.AddComponent<BoxColliderComponent>(32, 32);
-				enemy.AddComponent<ProjectileEmitterComponent>(glm::vec2(100.0, 0.0), 5000, 3000, 10, false);
-				enemy.AddComponent<HealthComponent>(100);
+				double projVelX = cos(projAngle) * projSpeed;
+				double projVelY = sin(projAngle) * projSpeed;
+				enemy.AddComponent<ProjectileEmitterComponent>(glm::vec2(projVelX, projVelY), projRepeat*1000, projDuration*1000, 10, false);
+				enemy.AddComponent<HealthComponent>(enemyHealth);
+				enemyXPos = enemyYPos = enemyXScale = enemyYScale = enemyXVel = enemyYVel = rotation = projAngle = 0;
+				projRepeat = projDuration = 10;
+				projSpeed = 100;
+				enemyHealth = 100;
 			}
+
+
+			
 		}
 
 		ImGui::End();
