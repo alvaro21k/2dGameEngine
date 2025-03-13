e7c63d4ee08c8ca8aabd75f37b031cfc589e67de
Alvaro Nicosia <alvaro21k@gmail.com>
2025-03-13 18:02:14 +0100

    Test functions with Lua

--- 2dGameEngine/assets/scripts/myscript.lua
+++ 2dGameEngine/assets/scripts/myscript.lua
@@ -3,10 +3,19 @@ some_variable = 7 * 9
 
 --This is a Lua Table
 config = {
-	title = "My Game Engine"
+	title = "My Game Engine",
 	fullscreen = false,
 	resolution = {
 		width = 800,
 		height = 600
 	}
 }
+
+function factorial(n)
+	if n == 1 then
+		return 1
+	end
+	return n * factorial(n-1)
+end
+
+print("The cube of the number 3 is equal to " .. cube(3))
--- 2dGameEngine/src/Main.cpp
+++ 2dGameEngine/src/Main.cpp
@@ -2,29 +2,15 @@
 #include "sol/sol.hpp"
 #include <iostream>
 
-void TestLua(){
-    sol::state lua;
-    lua.open_libraries(sol::lib::base);
-
-    lua.script_file("./assets/scripts/myscript.lua");
-    
-
-    int someVariableInsideCpp = lua["some_variable"];
-    std::cout << "The valuable of some_variable in C++ is " << someVariableInsideCpp << std::endl;
-
-    bool isFullScreen = lua["config"]["fullscreen"];
-}
-
 int main(int argc, char* argv[]) {
-    // TODO
-    /*Game game;
+     //TODO
+    Game game;
 
     game.Initialize();
    
     game.Run();
 
-    game.Destroy();*/
+    game.Destroy();
 
-    TestLua();
     return 0;
 }
